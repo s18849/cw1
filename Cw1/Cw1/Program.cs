8024a5c4d3e4e912cc02998e1677d6af8e448a3e
s18849 <s18849@pjwstk.edu.pl>
2020-03-12 18:45:01 +0100

    pobieranie maili ze strony

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("drugi commit");
+            string url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+            try
+            {
+                var client = new HttpClient();
+                var result = await client.GetAsync(url);
+                //ThreadPool() - trzyma gotowe do dzialania watki 
+
+                var list = new List<string>();
+                var zbior = new HashSet<string>();
+                var slownik = new Dictionary<string, int>();
+                if (result.IsSuccessStatusCode)
+                {
+                    string html = await result.Content.ReadAsStringAsync();
+                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+");
+                    var matches = regex.Matches(html);
+                    foreach (var m in matches)
+                    {
+                        Console.WriteLine(m);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string blad = string.Format("Wystapil blad {0}", ex.ToString());
+                Console.WriteLine($"Wystapil blad: {ex.ToString()}");
+            }
+
         }
+
+
     }
 }
